9893dad90c714ac56cb7da38d81786a0cd7461ae
Patrick Deubel <patrick.deubel@gmail.com>
2020-01-15 16:03:02 +0100

    Changed to keras instead of tf.keras and compiled model with loss.

--- ant-keras.py
+++ ant-keras.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import keras
 import numpy as np
 import os
 
@@ -28,12 +29,12 @@ w_2 = [take_weights_here["weights_dense2_w"], take_weights_here["weights_dense2_
 w_f = [take_weights_here["weights_final_w"], take_weights_here["weights_final_b"]]
 
 
-inputs = tf.keras.Input((28,))
-l_dense1 = tf.keras.layers.Dense(128, tf.nn.relu, use_bias=True, kernel_initializer=tf.keras.initializers.random_normal(), bias_initializer=tf.keras.initializers.random_normal())
+inputs = keras.Input((28,))
+l_dense1 = keras.layers.Dense(128, activation=tf.nn.relu, use_bias=True, kernel_initializer=keras.initializers.random_normal(), bias_initializer=keras.initializers.random_normal())
 #l_dense1.set_weights(w_1)
-l_dense2 = tf.keras.layers.Dense(64, tf.nn.relu, use_bias=True, kernel_initializer=tf.keras.initializers.random_normal(), bias_initializer=tf.keras.initializers.random_normal())
+l_dense2 = keras.layers.Dense(64, activation=tf.nn.relu, use_bias=True, kernel_initializer=keras.initializers.random_normal(), bias_initializer=keras.initializers.random_normal())
 #l_dense2.set_weights(w_2)
-l_dense3 = tf.keras.layers.Dense(8, use_bias=True, kernel_initializer=tf.keras.initializers.random_normal(), bias_initializer=tf.keras.initializers.random_normal())
+l_dense3 = keras.layers.Dense(8, use_bias=True, kernel_initializer=keras.initializers.random_normal(), bias_initializer=keras.initializers.random_normal())
 #l_dense3.set_weights(w_f)
 
 
@@ -41,7 +42,7 @@ l1 = l_dense1(inputs)
 l2 = l_dense2(l1)
 l3 = l_dense3(l2)
 
-model = tf.keras.Model(inputs=inputs, outputs=l3)
+model = keras.Model(inputs=inputs, outputs=l3)
 
 #test = model.get_layer(index=0)
 #model = tf.keras.models.Sequential()
@@ -53,5 +54,6 @@ model = tf.keras.Model(inputs=inputs, outputs=l3)
 model.get_layer(index=1).set_weights(w_1)
 model.get_layer(index=2).set_weights(w_2)
 model.get_layer(index=3).set_weights(w_f)
+model.compile(keras.optimizers.Adam(), loss=keras.losses.mean_squared_error)
 
-model.save("test-dir/agentzoo-conversion/keras-ant2.h5")
+model.save("test-dir/agentzoo-conversion/keras-ant-no_tf.h5")
